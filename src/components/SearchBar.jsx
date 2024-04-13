617dfb92b77d79c387c4e899896db89bfb4f1131
Akbarof01 <akhbbarov01@gmail.com>
2024-04-14 02:05:06 +0500

    7.3-2 youtube

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+
+  return (
+    <Paper
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        bgcolor: 'error.main',
+        borderRadius: 20,
+        border: '1px solid red',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className='search-bar'
+        placeholder='Qidiruv...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'white' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
